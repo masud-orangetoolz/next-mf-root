1bc3e9a59d4a71bfb45577bd2aa453eafc7ac152
masud-orangetoolz <masud.rana@orangetoolz.com>
2023-08-21 12:47:55 +0600

    setup: next mf-remote-app initial setup done

--- next.config.js
+++ next.config.js
@@ -6,13 +6,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  //   https://next-app-lhvy.vercel.app/
+  //   http://localhost:3001
+
   webpack(config, options) {
     const { isServer } = options;
+    console.log("process.env.NEXT_PUBLIC_REMOTE_APP", process.env.NEXT_PUBLIC_REMOTE_APP);
+
     config.plugins.push(
         new NextFederationPlugin({
           name: 'root',
           remotes: {
-              remoteApp: `remoteApp@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+              remoteApp: `remoteApp@${process.env.NEXT_PUBLIC_REMOTE_APP}/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
           },
           filename: 'static/chunks/remoteEntry.js',
           exposes: {},
