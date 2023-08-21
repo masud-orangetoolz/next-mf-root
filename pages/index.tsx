4062f16710fea0d947bd3b229020c6c858a6421c
masud-orangetoolz <masud.rana@orangetoolz.com>
2023-08-21 15:50:45 +0600

    feat: s3 image add

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,21 @@ export default function Page() {
                 <title>
                     iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple
                 </title>
+                <link rel="icon" href="/favicon1.png"/>
                 <meta
                     name="description"
                     content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
                 />
-                <link rel="icon" href="/favicon1.png"/>
-
-                <meta property="og:title" content="iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple"/>
                 <meta
                     property="og:description"
                     content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
                 />
-                <meta property="og:image" content="https://example.com/images/cool-page.jpg"/>
-                <meta property="og:url" content="https://example.com"/>
 
-                <meta property="twitter:image" content="https://example.com/images/cool-page.jpg"/>
+                <meta property="og:title" content="iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple"/>
+                <meta property="og:image" content="https://jobfalo.sfo3.digitaloceanspaces.com/images/jobfalo-logo.png"/>
+                <meta property="og:url" content="https://jobfalo.com" />
+
+                <meta property="twitter:image" content="https://jobfalo.sfo3.digitaloceanspaces.com/images/jobfalo-logo.png"/>
                 <meta property="twitter:card" content="iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple"/>
                 <meta property="twitter:title" content="iPhone 12 XS Max For Sale in Colorado - Big Discounts | Apple"/>
                 <meta property="twitter:description" content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."/>
